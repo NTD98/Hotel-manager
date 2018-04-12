8342edc3ed524b661a62b582133e141fd11f60bb
NTD <nhokcute533@gmail.com>
2018-04-12 12:43:58 +0700

    up

---
+++ Hotel-manager-master/QuanLyKhachSan/DTO/CustomerDTO.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.DTO
+{
+    public class CustomerDTO
+    {
+        private int customerCode;
+        private string customerName;
+        private int customerStyle;
+        private int customerCMND;
+        private string customerAddress;
+
+        public int CustomerCode
+        {
+            get
+            {
+                return customerCode;
+            }
+
+            set
+            {
+                customerCode = value;
+            }
+        }
+
+        public string CustomerName
+        {
+            get
+            {
+                return customerName;
+            }
+
+            set
+            {
+                customerName = value;
+            }
+        }
+
+        public int CustomerStyle
+        {
+            get
+            {
+                return customerStyle;
+            }
+
+            set
+            {
+                customerStyle = value;
+            }
+        }
+
+        public int CustomerCMND
+        {
+            get
+            {
+                return customerCMND;
+            }
+
+            set
+            {
+                customerCMND = value;
+            }
+        }
+
+        public string CustomerAddress
+        {
+            get
+            {
+                return customerAddress;
+            }
+
+            set
+            {
+                customerAddress = value;
+            }
+        }
+        public CustomerDTO() { }
+        public CustomerDTO( string customerName  , int customerStyle , int customerCMND ,string customerAddress, int customerCode = 6)
+        {
+            this.customerCode = customerCode;
+            this.customerName = customerName;
+            this.customerStyle = customerStyle;
+            this.customerCMND = customerCMND;
+            this.customerAddress = customerAddress;
+        }
+    }
+}
---
+++ Hotel-manager-master/QuanLyKhachSan/fViewRoom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyKhachSan.DAO;
+using QuanLyKhachSan.DTO;
+namespace QuanLyKhachSan
+{
+    public partial class fViewRoom : Form
+    {
+        public fViewRoom(List<BillInfoDTO> ListBillInfo,int roomcode)
+        {
+            InitializeComponent();
+            this.label1.Text = "Phòng " + roomcode.ToString();
+            foreach (BillInfoDTO item in ListBillInfo)
+                {
+                        this.label13.Text = item.CustomerName1;
+                        this.label12.Text = item.CustomerType1.ToString();
+                        this.label11.Text = item.IdentityCard1.ToString();
+                        this.label10.Text = item.DayCheckIn1.ToString();
+                        this.label9.Text = item.RoomType1.ToString();
+                        this.label8.Text = item.RoomNote1;
+                }
+        }
+    }
+}
